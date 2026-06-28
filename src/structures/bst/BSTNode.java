e2b518da79b06b5815496418f8566f178483eb89
fernando-222 <ortizortegafernando@gmail.com>
2026-06-28 17:15:55 -0500

    crear nodo genérico para el árbol binario

--- src/structures/bst/BSTNode.java
+++ src/structures/bst/BSTNode.java
@@ -1,5 +1,38 @@
 package structures.bst;
 
-public class BSTNode {
+public class BSTNode<T> {
 
+    private T data;
+    private BSTNode<T> left;
+    private BSTNode<T> right;
+
+    public BSTNode(T data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public BSTNode<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(BSTNode<T> left) {
+        this.left = left;
+    }
+
+    public BSTNode<T> getRight() {
+        return right;
+    }
+
+    public void setRight(BSTNode<T> right) {
+        this.right = right;
+    }
 }

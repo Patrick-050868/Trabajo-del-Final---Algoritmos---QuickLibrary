868355655d3928ef2d332c12cc3c9afac1e047cb
leroyroman11-ops <leroyroman.11@gmail.com>
2026-06-29 17:47:02 -0500

    Update MainMenuFrame.java

--- src/menu/MainMenuFrame.java
+++ src/menu/MainMenuFrame.java
@@ -54,6 +54,30 @@ public class MainMenuFrame extend JFrame {
           
               JOptionPane.showMessageDialog(this, "Libro registrado");
               break;
+
+            case "Mostrar libros":
+              String libros = bibliotecaService.mostrarLibros();
+
+              JTextArea area = new JTextArea(20, 40);
+              area.setText(libros);
+              area.setEditable(false);
+
+              JOptionPane.showMessageDialog(this, new JScrollPane(area));
+              break;
+      
+            case "Buscar libro por código":
+              String codBuscar = JOptionPane.showInputDialog("Código:");
+              String libro = bibliotecaService.buscarPorCodigo(codBuscar);
+
+              JOptionPane.showMessageDialog(this, libro);
+              break;
+
+            case "Buscar libros por categoría":
+              String cat = JOptionPane.showInputDialog("Categoría:");
+              String resultado = bibliotecaService.buscarPorCategoria(cat);
+
+              JOptionPane.showMessageDialog(this, resultado);
+              break;
         }
   }
 }

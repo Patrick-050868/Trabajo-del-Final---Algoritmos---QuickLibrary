26c7d031d029e7ca0253bac025d2c09d0f3d1beb
fernando-222 <ortizortegafernando@gmail.com>
2026-06-29 00:13:31 -0500

    implementar búsqueda de elementos en el árbol binario
    Se implementó la búsqueda recursiva de elementos en el árbol binario de búsqueda utilizando la comparación definida por Comparable.

--- src/structures/bst/BinarySearchTree.java
+++ src/structures/bst/BinarySearchTree.java
@@ -35,4 +35,34 @@ public class BinarySearchTree<T extends Comparable<T>> {
         // Si es igual, no inserta duplicados
         return node;
     }
+    // Método público para buscar un elemento
+    public T search(T data) {
+        BSTNode<T> result = search(root, data);
+    
+        if (result == null) {
+            return null;
+        }
+    
+        return result.getData();
+    }
+    
+    // Método recursivo privado
+    private BSTNode<T> search(BSTNode<T> node, T data) {
+    
+        if (node == null) {
+            return null;
+        }
+    
+        int comparacion = data.compareTo(node.getData());
+    
+        if (comparacion == 0) {
+            return node;
+        }
+    
+        if (comparacion < 0) {
+            return search(node.getLeft(), data);
+        }
+    
+        return search(node.getRight(), data);
+    }
 }

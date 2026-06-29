8cd7eb646c491192b459c3717f790d2d8020b621
HugoRQLL <huroquillan0525@gmail.com>
2026-06-29 17:42:03 -0500

    Enhance SolicitudPrestamo with new fields and methods
    Added fields and methods to handle loan requests.

--- src/model/SolicitudPrestamo.java
+++ src/model/SolicitudPrestamo.java
@@ -5,7 +5,33 @@ public class SolicitudPrestamo {
   private String codigoestudiante;
   private String nombreestudiante;
   private String codigolibro;
+  private String LocalDate fechasolicitud;
 
+  public solicitudprestamo (String codigoestudiante,String nombreestudiante, String codigolibro)
+  {
+    this.codigoestudiante = codigoestudiante;
+    this.nombreestudiante = nombreestudiante;
+    this.codigolibro = codigolibro;
+    this.fechasolicitud = LocalDate.now();
+  }
+
+  public String getcodigoestudiante(){
+    return codigoestudiante;
+  }
+
+  public void setcodigoestudiante(){
+    this.codigoestudiante = codigoestudiante;
+  
+  }
+  public String getnombreestudiante()
+  {
+    return nombreestudiante;
+  }
+  public void setnombreestudiante()
+  {
+    this.nombreestudiante = nombre estudiante;
+  }
+  public 
   
 
 }

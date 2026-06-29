6ce938096fb9beaea28f03206f344e4c2723f46e
fernando-222 <ortizortegafernando@gmail.com>
2026-06-28 19:42:28 -0500

    crear estructura base del árbol binario de búsqueda
    Se agregó la estructura inicial del árbol binario genérico con la raíz, el constructor y el método isEmpty().

--- src/structures/bst/BinarySearchTree.java
+++ src/structures/bst/BinarySearchTree.java
@@ -1,5 +1,15 @@
 package structures.bst;
 
-public class BinarySearchTree {
+public class BinarySearchTree<T extends Comparable<T>> {
+
+    private BSTNode<T> root;
+
+    public BinarySearchTree() {
+        this.root = null;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
 
 }

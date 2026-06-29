3efec5aa424720c12855c20064aa84acbc0517e5
fernando-222 <ortizortegafernando@gmail.com>
2026-06-28 23:58:38 -0500

    implementar inserción de elementos en el árbol binario
    Se implementó la operación de inserción recursiva para agregar elementos al árbol binario de búsqueda respetando el orden definido por Comparable.

--- src/structures/bst/BinarySearchTree.java
+++ src/structures/bst/BinarySearchTree.java
@@ -12,4 +12,27 @@ public class BinarySearchTree<T extends Comparable<T>> {
         return root == null;
     }
 
+    // Método público para insertar
+    public void insert(T data) {
+        root = insert(root, data);
+    }
+
+    // Método recursivo privado
+    private BSTNode<T> insert(BSTNode<T> node, T data) {
+
+        if (node == null) {
+            return new BSTNode<>(data);
+        }
+
+        int comparacion = data.compareTo(node.getData());
+
+        if (comparacion < 0) {
+            node.setLeft(insert(node.getLeft(), data));
+        } else if (comparacion > 0) {
+            node.setRight(insert(node.getRight(), data));
+        }
+
+        // Si es igual, no inserta duplicados
+        return node;
+    }
 }

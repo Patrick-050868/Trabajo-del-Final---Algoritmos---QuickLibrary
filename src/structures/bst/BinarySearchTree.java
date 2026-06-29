6220a7576c09eba43908ee043303d2dbd9c192e2
fernando-222 <ortizortegafernando@gmail.com>
2026-06-29 13:50:49 -0500

    agregar pruebas del árbol binario de búsqueda

--- src/structures/bst/BinarySearchTree.java
+++ src/structures/bst/BinarySearchTree.java
@@ -98,4 +98,58 @@ public class BinarySearchTree<T extends Comparable<T>> {
 
         return 1 + count(node.getLeft()) + count(node.getRight());
     }
+    
+    // Método público para eliminar un elemento
+    public void delete(T data) {
+        root = delete(root, data);
+    }
+
+    // Método recursivo privado
+    private BSTNode<T> delete(BSTNode<T> node, T data) {
+
+        if (node == null) {
+            return null;
+        }
+
+        int comparacion = data.compareTo(node.getData());
+
+        if (comparacion < 0) {
+
+            node.setLeft(delete(node.getLeft(), data));
+
+        } else if (comparacion > 0) {
+
+            node.setRight(delete(node.getRight(), data));
+
+        } else {
+
+            // Caso 1: no tiene hijo izquierdo
+            if (node.getLeft() == null) {
+                return node.getRight();
+            }
+
+            // Caso 2: no tiene hijo derecho
+            if (node.getRight() == null) {
+                return node.getLeft();
+            }
+
+            // Caso 3: tiene dos hijos
+            BSTNode<T> successor = findMin(node.getRight());
+
+            node.setData(successor.getData());
+
+            node.setRight(delete(node.getRight(), successor.getData()));
+        }
+
+        return node;
+    }
+    // Obtiene el menor elemento del subárbol
+    private BSTNode<T> findMin(BSTNode<T> node) {
+
+        while (node.getLeft() != null) {
+            node = node.getLeft();
+        }
+
+        return node;
+    }
 }

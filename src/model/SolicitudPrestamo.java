c2fbeaace9da846b91281f4a9d2440479979a9a3
HugoRQLL <huroquillan0525@gmail.com>
2026-06-28 21:33:52 -0500

    solo los privates
    adsadadsa

--- src/model/SolicitudPrestamo.java
+++ src/model/SolicitudPrestamo.java
@@ -1,5 +1,11 @@
 package model;
+import java.time.localDate
 
 public class SolicitudPrestamo {
+  private String codigoestudiante;
+  private String nombreestudiante;
+  private String codigolibro;
+
+  
 
 }

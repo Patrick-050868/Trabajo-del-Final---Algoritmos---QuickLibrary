ee87600156b345930d318645a135f16125156a9f
leroyroman11-ops <leroyroman.11@gmail.com>
2026-06-29 21:20:28 -0500

    Update ResporteService.java

--- src/service/ResporteService.java
+++ src/service/ResporteService.java
@@ -1,5 +1,22 @@
 package service;
 
+import model.EstadoLibro;
+
 public class ResporteService {
 
+    private final BibliotecaService bibliotecaService;
+    private final PrestamoService prestamoService;
+
+    public ReporteService(BibliotecaService bibliotecaService, PrestamoService prestamoService) {
+        this.bibliotecaService = bibliotecaService;
+        this.prestamoService = prestamoService;
+    }
+
+    public String generarReporte() {
+        return "REPORTE QUICKLIBRARY" + System.lineSeparator()
+                + "Total de libros: " + bibliotecaService.contarTotal() + System.lineSeparator()
+                + "Libros disponibles: " + bibliotecaService.contarPorEstado(EstadoLibro.DISPONIBLE) + System.lineSeparator()
+                + "Libros prestados: " + bibliotecaService.contarPorEstado(EstadoLibro.PRESTADO) + System.lineSeparator()
+                + "Solicitudes pendientes: " + prestamoService.contarPendientes();
+    }
 }
